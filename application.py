81b591f13fb36c8fd65eb992c9616f3733ee33d1
Adooz <kndonake@gmail.com>
2023-09-13 22:03:25 +0100

    port comfiguration

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
